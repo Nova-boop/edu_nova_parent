7cefb88ede7ae72a1e8c84855284485f009a8faf
Nova <king-cz@outlook.com>
2020-11-17 15:24:26 +0800

    讲师增删改查功能

---
+++ common/service_base/src/main/java/com/nova/servicebase/handler/MyMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.nova.servicebase.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 自动更新时间
+ */
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+
+    }
+}
--- service/service_edu/src/main/java/com/nova/eduService/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/nova/eduService/controller/EduTeacherController.java
@@ -1,12 +1,17 @@
 package com.nova.eduService.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.nova.commonutils.R;
 import com.nova.eduService.entity.EduTeacher;
+import com.nova.eduService.entity.vo.QueryTeacher;
 import com.nova.eduService.service.EduTeacherService;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -36,6 +41,16 @@ public class EduTeacherController {
         this.teacherService = teacherService;
     }
 
+    // 根据ID 查询讲师信息
+    @ApiOperation(value = "根据讲师ID查询讲师信息")
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id) {
+        EduTeacher teacher = teacherService.getById(id);
+        return R.ok().data("items", teacher);
+    }
+
     // 查询 edu_teacher 中所有的数据
     // restful 风格
     @GetMapping("finAllTeacher")
@@ -49,7 +64,7 @@ public class EduTeacherController {
     }
 
     // 根据ID逻辑删除讲师
-    @DeleteMapping("{id}")
+    @DeleteMapping("removeTeacher/{id}")
     @ApiOperation(value = "根据ID删除讲师")
     public R removeTeacher(
             @ApiParam(name = "id", value = "讲师ID", required = true)
@@ -66,14 +81,16 @@ public class EduTeacherController {
     @GetMapping("pageTeacher/{current}/{limit}")
     @ApiOperation(value = "分页查询讲师列表")
     public R PageListTeacher(
+            @ApiParam(name = "current",value = "当前页",required = true)
             @PathVariable Integer current,
+            @ApiParam(name = "limit",value = "每页显示数量",required = true)
             @PathVariable Integer limit) {
         // 创建Page 对象
         Page<EduTeacher> teacherPage = new Page<>(current, limit);
 
         // 获取数据病组织
         IPage<EduTeacher> teacherIPage = teacherService.page(teacherPage, null);
-        long total = teacherIPage.getTotal(); // 获取数据的总行数
+        long total = teacherIPage.getTotal(); // 获取数据的总数
         List<EduTeacher> records = teacherIPage.getRecords(); // 获取每一页数据的列表
         //返回数据
         // return R.ok().data("total",total).data("items",records);
@@ -81,6 +98,81 @@ public class EduTeacherController {
         map.put("total", total);
         map.put("rows", records);
         return R.ok().data(map);
+    }
+
+    // 多条件组合查询
+
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    @ApiOperation(value = "多条件查询讲师列表")
+    public R pageListTeacherCondition(
+            @ApiParam(name = "current",value = "当前页",required = true)
+            @PathVariable Integer current,
+            @ApiParam(name = "limit",value = "每页显示数量",required = true)
+            @PathVariable Integer limit,
+            @RequestBody(required = false) QueryTeacher queryTeacher) {
+        // 创建一个page 对象
+        Page<EduTeacher> eduTeacherPage = new Page<>(current, limit);
+
+        // 构建多条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+
+        String name = queryTeacher.getName();
+        Integer level = queryTeacher.getLevel();
+        String begin = queryTeacher.getBegin();
+        String end = queryTeacher.getEnd();
+
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name); // 模糊查询
+        }
+
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+
+        if (StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+
+        // 调用方法进行查询
+        IPage<EduTeacher> teacherIPage = teacherService.page(eduTeacherPage, wrapper);
+
+        long total = teacherIPage.getTotal(); // 获取数据的总数
+        List<EduTeacher> records = teacherIPage.getRecords(); // 获取每一页数据的列表
+        return R.ok().data("total", total).data("items", records);
+    }
+
+    // 添加讲师的接口
+    @PostMapping("addTeacher")
+    @ApiOperation(value = "添加讲师")
+    public R addTeacher(
+            @ApiParam(name = "teacher", value = "讲师对象", required = true)
+            @RequestBody EduTeacher eduTeacher) {
+        // 组织数据写入数据库
+        boolean saveResult = teacherService.save(eduTeacher);
+
+        // 返回结果
+        if (saveResult) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    @ApiOperation(value = "根据ID 修改讲师信息")
+    @PostMapping("updateTeacher")
+    public R updateTeacher(
+            @RequestBody EduTeacher eduTeacher) {
+        boolean flag = teacherService.updateById(eduTeacher);
+
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
 }
 
