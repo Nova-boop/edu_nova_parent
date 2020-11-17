a7bbfffd6d1b49681a47e388749be6cfa9e533bb
Nova <king-cz@outlook.com>
2020-11-17 17:02:25 +0800

    新增特定异常处理

--- common/service_base/src/main/java/com/nova/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/nova/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -6,15 +6,33 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 全局统一异常处理类
+ * 异常处理类
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // 全局统一异常处理类
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public R error(Exception e) {
         e.printStackTrace();
         return R.error().message("执行了全局异常...");
-
     }
+
+    // 特定异常 ArithmeticException
+    @ResponseBody
+    @ExceptionHandler(ArithmeticException.class)
+    public R error(ArithmeticException e) {
+        e.printStackTrace();
+        return R.error().message("执行了ArithmeticException异常...");
+    }
+
+    // 自定义异常处理
+    @ResponseBody
+    @ExceptionHandler(NovaException.class)
+    public R error(NovaException e) {
+        e.printStackTrace();
+        return R.error().code(e.getCode()).message(e.getMsg());
+    }
+
 }
