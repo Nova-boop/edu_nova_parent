5a5596ebca7e4d4ca3b0104d3ac4db6e014e4ee1
Nova <king-cz@outlook.com>
2020-11-20 18:31:09 +0800

    更新 aliyun-oss 中文件组织方式

--- service/service_oss/src/main/java/com/nova/ossService/service/impl/FileServiceImpl.java
+++ service/service_oss/src/main/java/com/nova/ossService/service/impl/FileServiceImpl.java
@@ -2,13 +2,14 @@ package com.nova.ossService.service.impl;
 
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.model.PutObjectResult;
 import com.nova.ossService.service.FileService;
 import com.nova.ossService.utils.ConstantPropertiesUtil;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -32,12 +33,24 @@ public class FileServiceImpl implements FileService {
             OSS ossClient = new OSSClientBuilder().build(endPoint, accessKeyId, accessKeySecret);
             // 上传文件流
             InputStream inputStream = file.getInputStream();
+            // 获取文件名
             String filename = file.getOriginalFilename();
+
+            // 添加uuid随机字符串避免文件被覆盖
+            String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+            String newName =uuid + filename ;
+
+            // 构建日期路径
+            String datePath = new DateTime().toString("yyyy/MM/dd");
+            filename = datePath + "/" + newName;
+
+            // 实现文件上传
             ossClient.putObject(bucketName, filename, inputStream);
 
             // 关闭OSSClient
             ossClient.shutdown();
 
+            // 拼接获取文件的url
             // https://edu-nova.oss-cn-chengdu.aliyuncs.com/01.jpg
             uploadUrl = "https://" + bucketName + "." + endPoint + "/" + filename;
         } catch (Exception ignored) {
