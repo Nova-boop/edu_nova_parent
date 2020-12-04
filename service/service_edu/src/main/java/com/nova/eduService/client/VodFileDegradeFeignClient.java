e4c14263d13f31cf4e1dcee4cb6695f51a721300
Nova <king-cz@outlook.com>
2020-12-04 11:28:50 +0800

    实现 熔断器

---
+++ service/service_edu/src/main/java/com/nova/eduService/client/VodFileDegradeFeignClient.java
@@ -0,0 +1,22 @@
+package com.nova.eduService.client;
+
+import com.nova.commonutils.Result;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class VodFileDegradeFeignClient  implements VodClient{
+
+    // 删除一个视频
+    @Override
+    public Result delVideo(String videoId) {
+        return Result.error().message("删除视频出错,服务器异常触发熔断");
+    }
+
+    // 删除多个视频
+    @Override
+    public Result delVideoList(List<String> videoList) {
+        return Result.error().message("删除视频出错,服务器异常触发熔断");
+    }
+}
