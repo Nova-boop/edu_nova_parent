0ea576b487907f76b471813c3b58a106e8d2846d
loval <yiwu_python@localhost>
2020-12-06 10:50:57 +0800

    update 熔断器

--- service/service_edu/src/main/java/com/nova/eduService/client/VodFileDegradeFeignClient.java
+++ service/service_edu/src/main/java/com/nova/eduService/client/VodFileDegradeFeignClient.java
@@ -6,17 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-<<<<<<< HEAD
-public class VodFileDegradeFeignClient implements VodClient {
-    @Override
-    public Result delVideo(String videoId) {
-        return Result.error().message("服务器异常 触发熔断");
-    }
 
-    @Override
-    public Result delVideoList(List<String> videoList) {
-        return Result.error().message("服务器异常 触发熔断");
-=======
 public class VodFileDegradeFeignClient  implements VodClient{
 
     // 删除一个视频
@@ -29,6 +19,5 @@ public class VodFileDegradeFeignClient  implements VodClient{
     @Override
     public Result delVideoList(List<String> videoList) {
         return Result.error().message("删除视频出错,服务器异常触发熔断");
->>>>>>> origin/main
     }
 }
