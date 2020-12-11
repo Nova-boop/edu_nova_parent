0bf1885998369077d98bf2fddcb9a50aac0a9b08
Nova <king-cz@outlook.com>
2020-12-11 17:54:06 +0800

    整理代码格式

--- service/service_edu/src/main/java/com/nova/eduService/client/VodClient.java
+++ service/service_edu/src/main/java/com/nova/eduService/client/VodClient.java
@@ -15,9 +15,9 @@ public interface VodClient {
 
     // 调用 nacos  根据视频ID删除阿里云视频
     @DeleteMapping("/eduVod/video/delVideo/{videoId}")
-    public Result delVideo(@PathVariable("videoId") String videoId);
+    Result delVideo(@PathVariable("videoId") String videoId);
 
     // 调用nacos 批量删除阿里云视频
     @DeleteMapping("/eduVod/video/delVideoList")
-    public Result delVideoList(@RequestParam(value = "videoList") List<String> videoList);
+    Result delVideoList(@RequestParam(value = "videoList") List<String> videoList);
 }
